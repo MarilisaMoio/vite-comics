3ce394fa0cc013c14b806391f7cd8e96a7fefed3
Marilisa Moio <moiomarilisa@gmail.com>
2024-04-11 19:48:37 +0200

    feat: add card section

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script>
 	import AppHeader from "./components/AppHeader.vue"
+	import AppCards from "./components/AppCards.vue"
 
 	export default {
 		components: {
 			AppHeader,
+			AppCards
 		}
 	}
 </script>
 
 <template>
 	<AppHeader></AppHeader>
+	<main>
+		<appCards></appCards>
+	</main>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppCards.vue
@@ -0,0 +1,26 @@
+<script>
+    export default{
+        name: "AppCards",
+    };
+</script>
+
+<template>
+    <section>
+        <div class="container">
+        --> content goes here <--
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+    @use "../style/partials/variables" as *;
+
+    section{
+        background-color: $secondary_dark;
+        color: white;
+        font-size: 2rem;
+        .container{
+            padding: 20px;
+        }
+    }
+</style>
